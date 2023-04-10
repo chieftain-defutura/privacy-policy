61afb9d9de84447ceeb56ef327dc726a4bf24cbc
subaa P <subaap86@gmail.com>
2023-04-10 16:27:19 +0530

    chore: base setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+    </Routes>
+  );
+}
+
+export default App;
