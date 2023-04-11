e5fcae91cc33077b884e43bc4259f0a1aaffff27
suganya05 <go2suganya@gmail.com>
2023-04-11 15:03:32 +0530

    chore: privacypolicy app remove

--- src/pages/PrivacyPolicyHome/index.tsx
+++ src/pages/PrivacyPolicyHome/index.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./PrivacyPolicyHome.scss";
 import HalfbgImg from "../../assets/images/half-background-image.png";
 import Header from "../../components/Header";
-import Amazon from "../../assets/images/amazon.png";
-import Appstore from "../../assets/images/appstore.png";
-import Google from "../../assets/images/googlestore.png";
 import PrivacyPolicy from "../PrivacyPolicy";
 
 const PrivacyPolicyHome: React.FC = () => {
@@ -23,12 +20,6 @@ const PrivacyPolicyHome: React.FC = () => {
               <div className="image">
                 <img src={HalfbgImg} alt="" />
               </div>
-            </div>
-
-            <div className="app">
-              <img src={Appstore} alt="" />
-              <img src={Google} alt="" />
-              <img src={Amazon} alt="" />
             </div>
           </div>
         </div>
