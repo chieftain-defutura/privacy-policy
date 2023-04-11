b5ad2386c9f31688a51850ec50a135fb8d02a5e3
subaa P <subaap86@gmail.com>
2023-04-11 18:39:33 +0530

    chore: header animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import SprinVerse from "../../assets/images/Sprin Verse.png";
 import "./Header.scss";
@@ -6,7 +7,7 @@ import "./Header.scss";
 import { ReactComponent as Menu } from "../../assets/icons/menu.svg";
 import { ReactComponent as Close } from "../../assets/icons/close.svg";
 
-const Header: React.FC = () => {
+const Header: React.FC<{}> = () => {
   const [active, setActive] = useState(0);
   const [activeColor, setActiveColor] = useState(false);
   const [open, setOpen] = useState(false);
@@ -44,36 +45,43 @@ const Header: React.FC = () => {
             </Link>
           </div>
         </div>
-        {open && (
-          <div className="side-bar">
-            <div className="side-logo">
-              <h1>SV</h1>
-            </div>
-            <div className="side-link" onClick={() => setActive(0)}>
-              <Link to="/">
-                <h2
-                  className={active === 0 ? "active-color" : "color"}
-                  onClick={() => setOpen(false)}
-                >
-                  Home
-                </h2>
-              </Link>
-            </div>
-            <div className="side-link" onClick={() => setActive(1)}>
-              <Link to="/privacy-policy" onClick={() => setOpen(false)}>
-                <h2
-                  onClick={() => setActiveColor(!activeColor)}
-                  className={active === 1 ? "active-color" : "color"}
-                >
-                  Privacy Policy
-                </h2>
-              </Link>
-            </div>
-            <div className="close" onClick={() => setOpen(false)}>
-              <Close />
-            </div>
-          </div>
-        )}
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              className="side-bar"
+              animate={{ top: 0, transitionDelay: "-200ms" }}
+              exit={{ top: -900 }}
+              initial={{ top: -300 }}
+            >
+              <div className="side-logo">
+                <h1>SV</h1>
+              </div>
+              <div className="side-link" onClick={() => setActive(0)}>
+                <Link to="/">
+                  <h2
+                    className={active === 0 ? "active-color" : "color"}
+                    onClick={() => setOpen(false)}
+                  >
+                    Home
+                  </h2>
+                </Link>
+              </div>
+              <div className="side-link" onClick={() => setActive(1)}>
+                <Link to="/privacy-policy" onClick={() => setOpen(false)}>
+                  <h2
+                    onClick={() => setActiveColor(!activeColor)}
+                    className={active === 1 ? "active-color" : "color"}
+                  >
+                    Privacy Policy
+                  </h2>
+                </Link>
+              </div>
+              <div className="close" onClick={() => setOpen(false)}>
+                <Close />
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
